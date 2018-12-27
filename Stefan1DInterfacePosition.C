a821e33386a849a53f1ab3d1ca4eea93a827f9b0
pblasiak <przemyslaw.blasiak@pwr.edu.pl>
2018-12-27 11:49:19 +0100

    First commit

---
+++ Stefan1DInterfacePosition.C
@@ -0,0 +1,212 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2008 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+2010-08-02 Eelco van Vliet: 1st public version of Stefan1DInterfacePosition:
+  http://www.cfd-online.com/Forums/openfoam-solving/66705-wallheatflux-bc-not-constant-after-restart.html#post269812
+
+2012-05-21 Eelco van Vliet:
+  Quoting: http://www.cfd-online.com/Forums/openfoam-post-processing/101972-wallheatflux-utility-incompressible-case.html#post362191
+  «modified the standard wallHeatflux utility which comes default with OF into
+  a version for incompressible flows. Also removed a bug out of the code.»
+
+2012-06-26 Eelco van Vliet:
+  Quoting: http://www.cfd-online.com/Forums/openfoam-post-processing/101972-wallheatflux-utility-incompressible-case.html#post368330
+  «p is now not required anymore.»
+
+2014-06-22: Bruno Santos: Adapted to OpenFOAM 2.2.x.
+
+-------------------------------------------------------------------------------
+License
+    This file is a derivative work of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    Stefan1DInterfacePosition
+
+Description
+    Calculates and writes the position of the interface for one dimensional
+	Stefan problem. 
+    Based on wallHeatFlux with changes to allow it on incompressible flows
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+// modified from  wallHeatFlux
+#include "singlePhaseTransportModel.H"
+#include "phaseChangeTwoPhaseMixture.H"
+#include "turbulentTransportModel.H"
+#include "turbulenceModel.H"
+#include "wallFvPatch.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+scalar func(scalar x, const scalar constValue)
+{
+	return x*Foam::exp(pow(x,2))*Foam::erf(x) - constValue;
+}
+
+scalar derivErf(const scalar x)
+{
+	return 2*Foam::exp(-pow(x,2))/Foam::sqrt(Foam::constant::mathematical::pi);
+}
+
+scalar derivFunc(scalar x)
+{
+	return 
+		Foam::exp(pow(x,2))*Foam::erf(x) 
+	  + x*(2*x*Foam::exp(pow(x,2))*Foam::erf(x) + Foam::exp(pow(x,2))*derivErf(x) ); 
+}
+
+dimensionedScalar deltaInter
+       (
+	       const scalar eps, 
+		   const dimensionedScalar thermCond, 
+		   const dimensionedScalar rho, 
+		   const dimensionedScalar cp, 
+		   const scalar time
+	   )
+{
+	return 2*eps*Foam::sqrt(thermCond*time/rho/cp);
+}
+
+int main(int argc, char *argv[])
+{
+
+    timeSelector::addOptions();
+    #include "setRootCase.H"
+#   include "createTime.H"
+    instantList timeDirs = timeSelector::select0(runTime, args);
+#   include "createMesh.H"
+#       include "createFields.H"
+	#include "readTransportProperties.H"
+
+	const label maxIterNr = 20000;
+	label iter = 0;
+	//const scalar Tw = 463.03;
+	//const scalar TSat = 453.03;
+	//const scalar cpv = 2.71E+003;
+	//const scalar rhov = 5.145;
+	//const scalar kappav = 0.0364;
+	//const scalar hfg = 2014580;
+	const scalar tol = 1e-12;
+	dimensionedScalar LHS;
+	scalar epsilon = 0;
+	scalar epsilonPrev = 0;
+
+	if (phaseChangeType == "evaporation")
+	{
+		LHS = cp2*(Tw - TSat)/hEvap/Foam::sqrt(Foam::constant::mathematical::pi);
+	}
+	else if (phaseChangeType == "condensation")
+	{
+		LHS = cp1*(TSat - Tw)/hEvap/Foam::sqrt(Foam::constant::mathematical::pi);
+	}
+	else
+	{
+		FatalErrorIn("Stefan1DInterfacePosition") 
+			<< "phaseChangeType can be \"evaporation\" or \"condensation\" only"
+			<< nl
+			<< exit(FatalError);
+	}
+
+	Info << "Type the starting value for epsilon:" << endl;	
+	std::cin >> epsilon;
+
+	do 
+	{
+		epsilonPrev = epsilon;		
+		epsilon -= func(epsilon, LHS.value())/derivFunc(epsilon);	
+		iter++;
+	} while ( mag(epsilon - epsilonPrev) > tol && iter < maxIterNr );
+
+    Info<< "Liczba iteracji: " << iter << endl;
+	Info<< "epsilon = " << epsilon << endl;
+
+	OFstream IFfile("IFposition.txt");
+	IFfile << "Time [s]\t" << "Numerical [m]\t" << "Analytical [m]\t" << "Error [%]" << endl;
+
+    forAll(timeDirs, timeI)
+    {
+        runTime.setTime(timeDirs[timeI], timeI);
+        Info<< "Time = " << runTime.timeName() << endl;
+        mesh.readUpdate();
+
+		Info<< "Reading field alpha.water\n" << endl;
+    	volScalarField alphal
+    	(
+    	    IOobject
+    	    (
+    	        "alpha.water",
+    	        runTime.timeName(),
+    	        mesh,
+    	        IOobject::MUST_READ,
+    	        IOobject::NO_WRITE
+    	    ),
+    	    mesh
+    	);
+
+        gradAlphal=fvc::grad(alphal);
+
+        Info<< endl;
+
+        dimensionedScalar interfacePosition("interfacePosition", dimLength, 0.0);
+   
+	    interfacePosition = sum(mag(gradAlphal)*mesh.C().component(0))/sum(mag(gradAlphal));
+
+	    Info<<"Interface position for time " 
+			<< runTime.timeName() 
+			<< " is equal to: " 
+			<< interfacePosition.value() 
+			<< " [m]"
+			<< endl;
+
+	    Info<< "\nSaving the results to IFposition.txt\n" << endl;
+
+		if (phaseChangeType == "evaporation")
+		{
+			IFfile << runTime.timeName() 
+	  		     << "\t" 
+	  			 << interfacePosition.value() 
+	  		     << "\t" 
+	  			 << deltaInter(epsilon, k2, rho2, cp2, runTime.value()).value() 
+	  		     << "\t" 
+				 << mag(interfacePosition.value() - deltaInter(epsilon,k2, rho2, cp2, runTime.value()).value())/
+						mag(deltaInter(epsilon, k2, rho2, cp2, runTime.value()).value() + VSMALL)*100
+	  			 << endl;
+		}
+		else
+		{
+			IFfile << runTime.timeName() 
+	  		     << "\t" 
+	  			 << interfacePosition.value() 
+	  		     << "\t" 
+	  			 << deltaInter(epsilon, k1, rho1, cp1, runTime.value()).value() 
+	  		     << "\t" 
+				 << mag(interfacePosition.value() - deltaInter(epsilon, k1, rho1, cp1, runTime.value()).value())/
+						mag(deltaInter(epsilon, k1, rho1, cp1, runTime.value()).value() + VSMALL)*100
+	  			 << endl;
+		}
+    }
+
+    Info<< "End" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
